749efd2d785d10bca8527d4b0bb0d96858717de6
Lateffi Tazri <m.lateffitazri@gmail.com>
2026-06-08 12:11:20 +0800

    chore: add supabase seed SQL

---
+++ supabase_seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO sellers (shop_name, whatsapp_number, taman_name, postcode, kawasan, badge, status, is_open, view_count, wa_click_count, testimonial_count)
+VALUES
+('Resepi Kak Mila', '60123456789', 'Taman Desa Baiduri', '41150', 'Klang', 'verified_seller', 'active', true, 45, 12, 8),
+('Dapur Kak Ros', '60198765432', 'Taman Desa Baiduri', '41150', 'Klang', 'seller_aktif', 'active', false, 23, 7, 5),
+('Dapur Budak Gemok', '60187654321', 'Taman Desa Baiduri', '41150', 'Klang', 'seller_baharu', 'active', false, 10, 3, 0);
