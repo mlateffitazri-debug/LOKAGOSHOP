b55bbffc4e82d601f867b3bb7eabb86f3853730d
Lateffi Tazri <m.lateffitazri@gmail.com>
2026-06-10 15:28:46 +0800

    feat: auto navigate back after QR download

--- src/app/sokong/page.tsx
+++ src/app/sokong/page.tsx
@@ -51,6 +51,7 @@ export default function FounderPage() {
     } catch { /* ignore */ }
 
     setDownloading(false)
+    window.history.length > 1 ? window.history.back() : (window.location.href = '/home')
   }
 
   return (
